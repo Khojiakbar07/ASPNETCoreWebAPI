d489305f1a3b6258061ea372a30703a8c92905fe
Zokirov Xojiakbar <hojiakbar312@gmail.com>
2024-11-18 18:52:56 +0500

    WIP

--- PokemonReviewApp/Data/DataContext.cs
+++ PokemonReviewApp/Data/DataContext.cs
@@ -11,11 +11,17 @@ namespace PokemonReviewApp.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
         public DbSet<Country> Countries { get; set; }
+
         public DbSet<Owner> Owners { get; set; }
+
         public DbSet<Pokemon> Pokemon { get; set; }
+
         public DbSet<PokemonOwner> PokemonOwners { get; set; }
+
         public DbSet<PokemonCategory> PokemonCategories { get; set; }
+
         public DbSet<Review> Reviews { get; set; }
         public DbSet<Reviewer> Reviewers { get; set; }
 
